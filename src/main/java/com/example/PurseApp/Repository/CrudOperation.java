a1c3e403ae0a8c324ef86a00d3847c5fd8f54c9f
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-14 20:34:34 +0300

    feat: create client repository

---
+++ src/main/java/com/example/PurseApp/Repository/CrudOperation.java
@@ -0,0 +1,15 @@
+package com.example.PurseApp.Repository;
+
+import jakarta.el.PropertyNotFoundException;
+
+import java.util.List;
+
+public interface CrudOperation<T> {
+    public List<T> findAll();
+    public List<T> saveAll(List<T> toSave);
+    public T save(T toSave);
+    public T update(T toUpdate);
+    T delete(T toDelete);
+    T getOne(T one) throws PropertyNotFoundException;
+
+}
