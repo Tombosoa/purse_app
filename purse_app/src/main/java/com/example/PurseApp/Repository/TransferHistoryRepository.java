ae9a998f1a353557ca391b1327959f8ef7b8c830
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-19 21:44:31 +0300

    feat: create transferHistory repository

---
+++ purse_app/src/main/java/com/example/PurseApp/Repository/TransferHistoryRepository.java
@@ -0,0 +1,60 @@
+package com.example.PurseApp.Repository;
+
+import com.example.PurseApp.DataBaseConnection;
+import com.example.PurseApp.Entity.TransferHistory;
+import jakarta.el.PropertyNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@Component
+public class TransferHistoryRepository implements CrudOperation<TransferHistory>{
+    String userName = System.getenv("DB_USERNAME");
+    String password = System.getenv("DB_PASSWORD");
+    String databaseName = System.getenv("DB_NAME");
+    DataBaseConnection dbConnection = new DataBaseConnection(userName, password, databaseName);
+    Connection conn = dbConnection.getConnection();
+    public Statement statement;
+    @Override
+    public List<TransferHistory> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<TransferHistory> saveAll(List<TransferHistory> toSave) {
+
+        return null;
+    }
+
+    @Override
+    public TransferHistory save(TransferHistory toSave) {
+        try{
+            String query = "INSERT INTO transferHistory (idtransactioncredited, idtransactiondebited) values (?,?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, toSave.getIdTransactionCredited());
+            preparedStatement.setInt(2, toSave.getIdTransactionDebited());
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return toSave;
+    }
+
+    @Override
+    public TransferHistory update(TransferHistory toUpdate) {
+        return null;
+    }
+
+    @Override
+    public TransferHistory delete(TransferHistory toDelete) {
+        return null;
+    }
+
+    @Override
+    public TransferHistory getOne(TransferHistory one) throws PropertyNotFoundException {
+        return null;
+    }
+}
