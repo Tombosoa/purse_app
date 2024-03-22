f480068d9197cc67d707734712f049bee2b02f7b
tokiniaina <hei.tokiniaina@gmail.com>
2024-03-22 09:03:16 +0300

    feat:add form so that we can add a new account

--- react-client/front/src/App.tsx
+++ react-client/front/src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./Pages/Dashboard";
 import Account from "./Pages/Account";
+import Formulaire from "./Pages/Formulaire";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/formulaire" element={<Formulaire/>}/>
+        
       </Routes>
     </>
   );
