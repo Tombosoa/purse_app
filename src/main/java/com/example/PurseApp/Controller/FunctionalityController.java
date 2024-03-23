103a046cb773d68aa89e3fa6a709eea1832367c5
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-23 23:22:56 +0300

    feat: create controller for scheduled, canceled and multitransfer method

--- src/main/java/com/example/PurseApp/Controller/FunctionalityController.java
+++ src/main/java/com/example/PurseApp/Controller/FunctionalityController.java
@@ -3,6 +3,7 @@ package com.example.PurseApp.Controller;
 import com.example.PurseApp.Entity.SupplyBody;
 import com.example.PurseApp.Entity.TransferHistory;
 import com.example.PurseApp.Functionnality.Functionality;
+import com.example.PurseApp.Repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +11,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 public class FunctionalityController {
     private final Functionality functionality;
+    private final TransactionRepository transactionRepository;
 
     @Autowired
-    public FunctionalityController(Functionality functionality) {
+    public FunctionalityController(Functionality functionality, TransactionRepository transactionRepository) {
         this.functionality = functionality;
+        this.transactionRepository = transactionRepository;
     }
 
     @PostMapping("/makeSupply")
@@ -33,5 +38,31 @@ public class FunctionalityController {
             @RequestParam UUID idAccountCredited
     ){
         return functionality.makeTransfer(amount, idAccountCredited,  idAccountDebited);
+    }
+
+    @PostMapping("/multiTransfer")
+    public List<TransferHistory> multiTransfer(
+            @RequestParam double amount,
+            @RequestParam UUID idAccountDebited,
+            @RequestParam List<UUID> idAccountCreditedList
+    ){
+        return functionality.multiTransfer(amount, idAccountDebited, idAccountCreditedList);
+    }
+
+    @PostMapping("/scheduledTransfer")
+    public TransferHistory scheduledTransfer(
+            @RequestParam double amount,
+            @RequestParam UUID idAccountDebited,
+            @RequestParam UUID idAccountCredited,
+            @RequestParam LocalDate newEffectiveDate
+            ){
+        return functionality.scheduledTransfer(amount, idAccountCredited, idAccountDebited, newEffectiveDate);
+    }
+
+    @PostMapping("/canceledTransaction")
+    public String canceledTransaction(
+              @RequestParam int id
+    ){
+        return transactionRepository.canceledTransaction(id);
     }
 }
