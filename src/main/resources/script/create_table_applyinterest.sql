4d796e716e250e9db02ab5ba456aed889b0477f2
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-23 23:20:19 +0300

    feat: add a new column for applyinterest table

--- src/main/resources/script/create_table_applyinterest.sql
+++ src/main/resources/script/create_table_applyinterest.sql
@@ -3,3 +3,7 @@ CREATE TABLE IF NOT EXISTS ApplyInterest(
     idInterest int references Interest(id),
     idAccount uuid references Account(id)
 );
+
+ALTER TABLE applyinterest add column startDate timestamp with time zone DEFAULT current_timestamp;
+ALTER TABLE applyinterest add column firstDue double precision;
+ALTER TABLE applyinterest add column actualDue double precision;
