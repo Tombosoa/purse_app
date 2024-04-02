16f83b65d83a22f8f0984de2c8077ffb12d2a24d
Tombosoa <hei.nantenaina.4@gmail.com>
2024-04-02 18:28:15 +0300

    fix: update sql function

--- src/main/resources/function/create_function_for_the_expense_and_cash_receipts_chart.sql
+++ src/main/resources/function/create_function_for_the_expense_and_cash_receipts_chart.sql
@@ -3,25 +3,34 @@ CREATE OR REPLACE FUNCTION SumIncomesAndExpenses(
     _endDate DATE,
     _accountId UUID
 ) RETURNS TABLE (
-    category_id INTEGER,
-    category_name category_enum,
-    operation_type TEXT,
-    total_amount DOUBLE PRECISION
+    month INTEGER,
+    year INTEGER,
+    Income DOUBLE PRECISION,
+    Expense DOUBLE PRECISION
 ) AS $$
 BEGIN
     RETURN QUERY
     SELECT
-        c.id AS category_id,
-        c.name AS category_name,
-        CASE WHEN SUM(CASE WHEN t.amount < 0 THEN t.amount ELSE 0 END) < 0 THEN 'Income' ELSE 'Expenses' END AS operation_type,
-        SUM(t.amount) AS total_amount
-    FROM Transaction t
-    JOIN Category c ON t.id_category = c.id
-    WHERE t.registration_date >= _startDate
-    AND t.registration_date <= _endDate
-    AND t.id_account = _accountId
-    GROUP BY c.id, c.name;
+        CAST(EXTRACT(MONTH FROM transaction.effective_date) AS INTEGER) AS month,
+        CAST(EXTRACT(YEAR FROM transaction.effective_date) AS INTEGER) AS year,
+        SUM(CASE WHEN transaction.description = 'Credited account' THEN amount ELSE 0 END) as Income,
+        SUM(CASE WHEN transaction.description = 'Debited account' THEN amount ELSE 0 END) as Expense
+    FROM
+        transaction
+    INNER JOIN
+        account ON account.id = transaction.id_account
+    INNER JOIN
+        category ON category.id = transaction.id_category
+    WHERE
+        account.id = _accountId
+        AND (
+            (_startDate IS NULL OR transaction.effective_date >= COALESCE(_startDate, '1900-01-01'))
+            AND (_endDate IS NULL OR transaction.effective_date <= COALESCE(_endDate, '9999-12-31'))
+        )
+    GROUP BY
+        month,
+        year
+    ORDER BY
+        month;
 END;
 $$ LANGUAGE plpgsql;
-GRANT SELECT ON TABLE Transaction TO postgres;
-GRANT EXECUTE ON FUNCTION SumIncomesAndExpenses(DATE, DATE, UUID) TO postgres;
